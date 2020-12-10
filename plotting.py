620fd728958bd75fbefe0b10454a7deeff253734
Aaron Brown <DaBrownDev@users.noreply.github.com>
2020-12-10 12:56:28 -0500

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-#import matplotlib.pyplot as plt
+
+import matplotlib.pyplot as plt
 import re
-time_rtt = []
+import numpy as np
+time = []
+data = []
 
 with open('clientQUIC-rtt2.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
         line_list = re.split("\t|\n", line)
-        time_rtt.append((line_list[-4], line_list[-2]))
+        time.append(line_list[-4])
+        data.append(line_list[-2])
 
-# time_rtt (current_time, rtt)
+print (time)
+print (data)
+graph_type = 'RTT'
+ind = np.arange(len(time)) 
+width = 0.1
+plt.bar(ind, data, width, label=graph_type)
+plt.ylabel(graph_type)
+plt.title('Time VS ' + graph_type)
+plt.savefig('plot_out.png')
